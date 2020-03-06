8a32fe69ea4fee9752adf63ae50ef9a4ce70b52d
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-03-06 15:52:13 +0530

    REFACTORD:Method Change To Support No Parameter And Added Constructors

--- src/main/java/MoodAnalyzer.java
+++ src/main/java/MoodAnalyzer.java
@@ -1,10 +1,23 @@
 public class MoodAnalyzer {
+   //DECLARING FIELDS
+    String message="";
+
+    //DEFAULT CONSTRUCTOR
+    public MoodAnalyzer() {
+   }
+
+   //PARAMETRISED CONSTRUCTOR
+   public MoodAnalyzer(String message) {
+        this.message=message;
+    }
+
+    //MAIN METHOD
     public static void main(String[] args) {
         System.out.println("Welcome To Mood Analyzer Problem");
     }
 
     //METHOD TO ANALYZE MESSAGE
-    public String analyzeMood(String message) {
+    public String analyzeMood() {
         if (message.contains("Sad")) {
             return "SAD";
         } else {
--- src/test/java/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest.java
@@ -2,20 +2,19 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyzerTest {
-    //CREATED OBJECT OF MOOD ANALYZER CLASS
-    MoodAnalyzer moodAnalyzer=new MoodAnalyzer();
-
     //TEST CASE TO CHECK SAD MOOD
     @Test
     public void giveMessage_WhenSad_ThenReturnSad() {
-        String mood = moodAnalyzer.analyzeMood("i am in Sad Mood");
+        MoodAnalyzer moodAnalyzer=new MoodAnalyzer("I am in Sad Mood");
+        String mood = moodAnalyzer.analyzeMood();
         Assert.assertEquals("SAD",mood);
     }
 
     //TEST CASE TO CHECK ANY MOOD
     @Test
     public void giveMessage_WhenNotSad_ThenReturnHappy() {
-        String mood = moodAnalyzer.analyzeMood("i am in Any Mood");
+        MoodAnalyzer moodAnalyzer=new MoodAnalyzer("I am in Happy Mood");
+        String mood = moodAnalyzer.analyzeMood();
         Assert.assertEquals("HAPPY",mood);
     }
 }
