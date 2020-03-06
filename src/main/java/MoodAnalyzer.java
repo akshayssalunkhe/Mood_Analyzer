3622e3ebe9bf9432604bb81c2ed0d5ded597adbc
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-03-06 12:23:13 +0530

    ADDED:Method To Analyze Mood Message

--- src/main/java/MoodAnalyzer.java
+++ src/main/java/MoodAnalyzer.java
@@ -2,4 +2,13 @@ public class MoodAnalyzer {
     public static void main(String[] args) {
         System.out.println("Welcome To Mood Analyzer Problem");
     }
+
+    //METHOD TO ANALYZE MESSAGE
+    public String analyzeMood(String message) {
+        if (message.contains("Sad")) {
+            return "SAD";
+        } else {
+            return "HAPPY";
+        }
+    }
 }
--- src/test/java/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest.java
@@ -1,2 +1,21 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MoodAnalyzerTest {
+    //CREATED OBJECT OF MOOD ANALYZER CLASS
+    MoodAnalyzer moodAnalyzer=new MoodAnalyzer();
+
+    //TEST CASE TO CHECK SAD MOOD
+    @Test
+    public void giveMessage_WhenSad_ThenReturnSad() {
+        String mood = moodAnalyzer.analyzeMood("i am in Sad Mood");
+        Assert.assertEquals("SAD",mood);
+    }
+
+    //TEST CASE TO CHECK ANY MOOD
+    @Test
+    public void giveMessage_WhenNotSad_ThenReturnHappy() {
+        String mood = moodAnalyzer.analyzeMood("i am in Any Mood");
+        Assert.assertEquals("HAPPY",mood);
+    }
 }
